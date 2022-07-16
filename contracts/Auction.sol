c373afaf3206241e4926e3432d0930aad7ad24a4
Rick Farina <rickrfarina@gmail.com>
2022-07-16 13:40:04 -0500

    Initial commit with initial testing

---
+++ contracts/Auction.sol
@@ -0,0 +1,217 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+interface IERC721 {
+    function transferFrom(address from, address to, uint nftId) external;
+}
+contract Auction {
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+
+    address payable public seller;
+    uint256 public startingBid;
+    uint256 public currentBid;
+    uint256 public auctionEndTime;
+    address payable public  highestBidder;
+    bool public auctionActive;
+
+    mapping(address => uint256) public bidderToBid;
+
+    Bid[] public bids;
+
+    struct Bid {
+        address bidder;
+        uint256 bid;
+    }
+
+    error InsufficientBidAmount(uint256 bidAmount, uint256 currentBid);
+    error AuctionExpired(uint256 currentTime, uint256 auctionEndTime);
+    error InsufficientFunds(uint256 bidAmount, uint256 currentBalance);
+
+    
+    
+    event Start(uint startingBid, uint duration);
+    event BidAccepted(address bidder, uint256 amount, uint256 bidTime);
+    event BidNotAccepted(address bidder, uint256 amount, uint256 bidTime);
+
+    event LosingBidReturned(address returnedTo, uint amount, uint256 returnTime);
+    event LosingBidNotReturned(address notReturnedTo, uint amount, uint256 notReturnTime);
+
+    event PaymentReceived(address sender, uint256 amount, string processedBy);
+
+    event AttemptingPaymentThatWillFail(
+        address sender,
+        uint256 amount,
+        string processedBy
+    );
+    event End(address winner, uint winningBid );
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Not seller");
+        _;
+    }
+
+    modifier onlySufficientBalance(address _sender, uint256 _amount) {
+        require(
+            msg.sender.balance >= msg.value,
+            "Payment rejected for lack of balance"
+        );
+        _;
+    }
+    modifier onlyAuctionActive() {
+        require(auctionActive, "Auction has ended");
+        _;
+    }
+
+    constructor(address _nft, uint _nftId) {
+        
+        nft = IERC721(_nft);
+        nftId = _nftId;
+
+        seller = payable(msg.sender);
+
+    }
+
+    function start(uint256 _startingBid, uint256 _auctionDuration) external onlySeller {
+        require(!auctionActive, "Already Started");
+
+        auctionActive = true;
+        auctionEndTime = block.timestamp + _auctionDuration;
+        startingBid = _startingBid;
+        currentBid = _startingBid;
+
+        // Transfer ownership of nft to this contract, so that it can sell it
+        nft.transferFrom(seller, address(this), nftId);
+
+        emit Start(startingBid, _auctionDuration);
+    }
+
+    function auctionStatus() external view returns (bool) {
+        return auctionActive;
+    }
+
+    /* 
+        @dev 
+            receive bid amount provided 
+                bid is > current bid
+                auction has not ended
+    */
+    function bid() external payable onlyAuctionActive {
+        uint256 _bidAmount = msg.value;
+
+        if (!(block.timestamp <= auctionEndTime)) {
+            revert AuctionExpired({
+                currentTime: block.timestamp,
+                auctionEndTime: auctionEndTime
+            });
+        }
+
+        if (!(_bidAmount > currentBid)) {
+            revert InsufficientBidAmount({
+                bidAmount: _bidAmount,
+                currentBid: currentBid
+            });
+        }
+
+        // Reduce prior bid liability
+        _returnOldBid(highestBidder, bidderToBid[highestBidder]);
+        
+
+        // Bid accepted
+        currentBid = _bidAmount;
+        highestBidder = payable(msg.sender);
+
+        // Add to mapping
+        bidderToBid[msg.sender] += _bidAmount;
+
+        // Add to array as Struct
+        bids.push(Bid(msg.sender, _bidAmount));
+
+        emit BidAccepted(msg.sender, _bidAmount, block.timestamp);
+    }
+
+    function _returnOldBid (address _priorBidder, uint _priorBidAmount) internal {
+        // Send back previous bids to previous highest bidder if not address(0)
+        if (highestBidder != address(0) && bidderToBid[_priorBidder] >= _priorBidAmount){
+
+            // Todo: should protect against reentrancy!!
+            bool success = payable(_priorBidder).send(_priorBidAmount);
+            if (!success) {
+                // Do not reduce liability
+                emit LosingBidNotReturned(_priorBidder, _priorBidAmount, block.timestamp);
+            } else {
+                // Reduce liability
+                bidderToBid[_priorBidder] -= _priorBidAmount; // reduce bid liability (s/b zero after)
+                emit LosingBidReturned(_priorBidder, _priorBidAmount, block.timestamp);
+            }
+        }
+    }
+
+    function endBid()
+        external
+        onlySeller
+        onlyAuctionActive
+        returns (address _winner, uint256 _winningBid)
+    {
+        auctionActive = false;
+        returnLosingBids();
+        _winner = highestBidder;
+        _winningBid = currentBid;
+
+        emit End(_winner, _winningBid);
+    }
+
+    function returnLosingBids() internal returns (bool) {
+        for (uint256 i = 0; i < bids.length; i++) {
+            Bid storage _bid = bids[i];
+            if (_bid.bidder != highestBidder) {
+                bool successfulSend = payable(_bid.bidder).send(_bid.bid);
+                if (successfulSend) {
+                    // Reduce amount bid by this address
+                    bidderToBid[_bid.bidder] -= _bid.bid;
+
+                    // Delete mapping if address contains 0 balance
+                    if (bidderToBid[_bid.bidder] == 0) {
+                        delete bidderToBid[_bid.bidder];
+                        // Code to remove struct from array if it no longer valuable
+                        // TBD, as it may be valuable for historical and reporting purposes
+                        delete bids[i];
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    function senderBalance() external view returns (uint256) {
+        return msg.sender.balance;
+    }
+
+    function contractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function auctionTimeRemaining()
+        external
+        view
+        returns (uint256 _auctionTimeRemaining, bool _auctionActive)
+    {
+        uint256 _currentTime = block.timestamp;
+        if (auctionEndTime > _currentTime) {
+            _auctionTimeRemaining = auctionEndTime - _currentTime;
+            _auctionActive = true;
+        } else {
+            _auctionTimeRemaining = 0;
+            _auctionActive = false;
+        }
+        return (_auctionTimeRemaining, _auctionActive);
+    }
+
+    receive() external payable onlySufficientBalance(msg.sender, msg.value) {
+        emit PaymentReceived(msg.sender, msg.value, "receive function");
+    }
+
+    fallback() external payable onlySufficientBalance(msg.sender, msg.value) {
+        emit PaymentReceived(msg.sender, msg.value, "fallback function");
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,30 @@
+const Nft = artifacts.require("Nft")
+const Auction = artifacts.require("Auction")
+const { writeFileSync } = require("fs")
+const path = "./deployedAddresses.json"
+
+module.exports = async function (deployer, network, accounts) {
+    if (network == "test") { return }  // test will deploy its own
+
+    // Deploy nft
+    console.log("\nDeploy Nft and Auction Contracts (2_deploy_contracts.js)")
+    
+    await deployer.deploy(Nft)
+    const nft = await Nft.deployed()
+    console.log(`nft.address:       ${nft.address}`)
+
+    const nftId = 777
+    // await nft.mint(deployer, nftId);  // mint the nft // causes error during deploy
+
+    // Deploy Auction (note there are 3 params)
+    await deployer.deploy(Auction, nft.address, nftId)
+    const auction = await Auction.deployed()
+    console.log(`auction.address:   ${auction.address}`)
+
+    const addresses = {
+        nft: nft.address,
+        auction: auction.address
+    }
+
+    writeFileSync(path, JSON.stringify(addresses,null,2), "utf-8")
+}
